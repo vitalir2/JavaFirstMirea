912cd299cd50f594253e824891ced53a2361ff93
vitalir2 <vitalyi100@mail.ru>
2021-10-07 19:42:05 +0300

    Complete 14th except 5th part

---
+++ src/ru/mirea/task14/CardGameConsole.java
@@ -0,0 +1,46 @@
+package ru.mirea.task14;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class CardGameConsole {
+
+    private static final int NUMBER_OF_CARDS = 10;
+    private static final Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Stack<Integer> firstPlayerCards = new Stack<>();
+        Stack<Integer> secondPlayerCards = new Stack<>();
+        String[] playerOneDeck = reader.nextLine().split(" ");
+        String[] playerTwoDeck = reader.nextLine().split(" ");
+        for (int i = 0; i < NUMBER_OF_CARDS / 2; ++i) {
+            firstPlayerCards.push(Integer.parseInt(playerOneDeck[NUMBER_OF_CARDS / 2 - i - 1]));
+        }
+        for (int i = 0; i < NUMBER_OF_CARDS / 2; ++i) {
+            secondPlayerCards.push(Integer.parseInt(playerTwoDeck[NUMBER_OF_CARDS / 2 - i - 1]));
+        }
+        int turnCount = 0;
+        while (!firstPlayerCards.empty() && !secondPlayerCards.empty()) {
+            if (turnCount >= 106) {
+                System.out.println("botva");
+                break;
+            }
+            int firstPlayerCard = firstPlayerCards.pop();
+            int secondPlayerCard = secondPlayerCards.pop();
+            if (firstPlayerCard < secondPlayerCard || (firstPlayerCard == 9 && secondPlayerCard == 0)) {
+                secondPlayerCards.add(0, firstPlayerCard);
+                secondPlayerCards.add(0, secondPlayerCard);
+            } else {
+                firstPlayerCards.add(0, firstPlayerCard);
+                firstPlayerCards.add(0, secondPlayerCard);
+            }
+            ++turnCount;
+        }
+        if (firstPlayerCards.empty()) {
+            System.out.println("second " + turnCount);
+        } else if (secondPlayerCards.empty()) {
+            System.out.println("first " + turnCount);
+        }
+    }
+}
---
+++ src/ru/mirea/task14/CardGameConsoleDeque.java
@@ -0,0 +1,44 @@
+package ru.mirea.task14;
+
+import java.util.*;
+
+public class CardGameConsoleDeque {
+    private static final int NUMBER_OF_CARDS = 10;
+    private static final Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Deque<Integer> firstPlayerCards = new LinkedList<>();
+        Deque<Integer> secondPlayerCards = new LinkedList<>();
+        // Duplicate...
+        String[] playerOneDeck = reader.nextLine().split(" ");
+        String[] playerTwoDeck = reader.nextLine().split(" ");
+        for (int i = 0; i < NUMBER_OF_CARDS / 2; ++i) {
+            firstPlayerCards.add(Integer.parseInt(playerOneDeck[i]));
+        }
+        for (int i = 0; i < NUMBER_OF_CARDS / 2; ++i) {
+            secondPlayerCards.add(Integer.parseInt(playerTwoDeck[i]));
+        }
+        int turnCount = 0;
+        while (!firstPlayerCards.isEmpty() && !secondPlayerCards.isEmpty()) {
+            if (turnCount >= 106) {
+                System.out.println("botva");
+                break;
+            }
+            int firstPlayerCard = firstPlayerCards.remove();
+            int secondPlayerCard = secondPlayerCards.remove();
+            if (firstPlayerCard < secondPlayerCard || (firstPlayerCard == 9 && secondPlayerCard == 0)) {
+                secondPlayerCards.add(firstPlayerCard);
+                secondPlayerCards.add(secondPlayerCard);
+            } else {
+                firstPlayerCards.add(firstPlayerCard);
+                firstPlayerCards.add(secondPlayerCard);
+            }
+            ++turnCount;
+        }
+        if (firstPlayerCards.isEmpty()) {
+            System.out.println("second " + turnCount);
+        } else if (secondPlayerCards.isEmpty()) {
+            System.out.println("first " + turnCount);
+        }
+    }
+}
