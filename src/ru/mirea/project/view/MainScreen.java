49df698763ce9d6a8601da2ac468a7656e822936
vitalir2 <vitalyi100@mail.ru>
2021-12-05 17:13:21 +0300

    Complete 24th practice

--- src/ru/mirea/project/view/MainScreen.java
+++ src/ru/mirea/project/view/MainScreen.java
@@ -5,16 +5,23 @@ import ru.mirea.project.core.db.UserDatabase;
 import ru.mirea.project.core.exceptions.InvalidDateFormatException;
 import ru.mirea.project.model.*;
 
+import java.security.InvalidParameterException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class MainScreen {
     // Now without any graphics, later I'm going to add some
+
+    private static final String DATE_FORMAT_PATTERN = "dd.MM.yyyy";
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat(DATE_FORMAT_PATTERN);
 
     private static final Scanner reader = new Scanner(System.in);
     private static final UserDatabase userDatabase = UserDatabase.getInstance();
@@ -23,20 +30,40 @@ public class MainScreen {
 
     public static void main(String[] args) {
         System.out.println("You need to create the user");
+        System.out.println("Enter your first name");
+        String firstName = reader.nextLine();
+        System.out.println("Enter your last name");
+        String lastName = reader.nextLine();
+        System.out.println("Enter your gender in format: M or W or T"); // T == they
+        String genderString = reader.nextLine();
+        Pattern genderPattern = Pattern.compile("[MWT]");
+        Gender gender = null;
+        if (genderPattern.matcher(genderString).matches()) {
+            switch (genderString.toCharArray()[0]) {
+                case 'M' -> gender = Gender.MALE;
+                case 'W' -> gender = Gender.FEMALE;
+                case 'T' -> gender = Gender.OTHER;
+            }
+        } else {
+            throw new InvalidParameterException("Invalid gender, please enter the valid one");
+        }
         System.out.println("Enter your birth date in a format: dd.mm.yyyy");
         String inputDate = reader.nextLine();
+        int age;
+        LocalDate validatedBirthDate = null;
         try {
-            validateDateFormat(inputDate);
-        } catch(InvalidDateFormatException exception) {
+            validatedBirthDate = validateDateFormatAndReturnDate(inputDate);
+        } catch (InvalidDateFormatException exception) {
             System.err.println(exception.getLocalizedMessage());
             System.exit(0);
         }
-
-        currentUser = new Patient("fullName", 12, Gender.MALE, LocalDate.now().toString());
+        LocalDate currentDate = LocalDate.now(ZoneId.systemDefault());
+        age = (int) ChronoUnit.YEARS.between(validatedBirthDate, currentDate);
+        currentUser = new Patient(firstName + " " + lastName, age, gender, currentDate.toString());
         userDatabase.add(currentUser.getUid(), currentUser);
-        System.out.println("New user has been created");
+        System.out.println("New user has been created: " + currentUser);
 
-        while(true) {
+        while (true) {
             showTasksChoicePatient();
             int choice = reader.nextInt();
             processPatientTaskChoice(choice);
@@ -94,9 +121,16 @@ public class MainScreen {
     }
 
     private static void validateDateFormat(String date) throws InvalidDateFormatException {
-        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
         try {
-            format.parse(date);
+            DATE_FORMAT.parse(date);
+        } catch (ParseException e) {
+            throw new InvalidDateFormatException(date);
+        }
+    }
+
+    private static LocalDate validateDateFormatAndReturnDate(String date) throws InvalidDateFormatException {
+        try {
+            return DATE_FORMAT.parse(date).toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
         } catch (ParseException e) {
             throw new InvalidDateFormatException(date);
         }
